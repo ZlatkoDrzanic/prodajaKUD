570ff52e939e008146469bc49e55ce10904c4ad3
ZlatkoDrzanic <zlatko8@gmail.com>
2026-06-11 15:43:47 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ── Verzija cache-a — promijenite ovu vrijednost za forsiranje novog cache-a ──
-const VERSION = 'v8';
+const VERSION = 'v9';
 const CACHE   = 'evidencija-pica-' + VERSION;
 
 const STATIC = [
